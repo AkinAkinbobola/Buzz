d0c14a924f79ed8189b9afb6e4424a1c4883d075
Akin Akinbobola <akinbobola71@gmail.com>
2023-06-13 19:02:29 +0100

    Update results

--- lib/results.dart
+++ lib/results.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'package:buzz/buzz.dart';
-import 'package:buzz/models/SpotifySongModel.dart';
 import 'package:flutter/material.dart';
 
+import 'models/SpotifySongModel.dart';
+
 class Results extends StatefulWidget {
-  const Results({super.key, required this.jsonData});
+  const Results({Key? key, required this.jsonData}) : super(key: key);
   final String jsonData;
 
   @override
@@ -11,13 +13,37 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
+  late SpotifySongModel songData;
+
+  @override
+  void initState() {
+    super.initState();
+    songData = spotifySongModelFromJson(widget.jsonData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        child: Text(widget.jsonData),
+        child: Column(
+          children: [
+            Text('Song ID: ${songData.id ?? "N/A"}'),
+            Text('Song name: ${songData.name ?? "N/A"}'),
+            Text('Album: ${songData.album?.name ?? "N/A"}'),
+            Text('Album ID: ${songData.album?.id ?? "N/A"}'),
+            Text('Year Released: ${songData.album?.releaseDate ?? "N/A"}'),
+            Text('Artists: ${getArtistsNames(songData.artists)}'),
+          ],
+        ),
       ),
     );
+  }
+
+  String getArtistsNames(List<SpotifySongModelArtist>? artists) {
+    if (artists == null || artists.isEmpty) {
+      return "N/A";
+    }
+    return artists.map((artist) => artist.name ?? "").join(", ");
   }
 }
