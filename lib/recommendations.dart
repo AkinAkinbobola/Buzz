6b88d1ee6d8f95e43d099c593cfa33f2050aae66
Akin Akinbobola <akinbobola71@gmail.com>
2023-06-25 19:38:49 +0100

    Rec

--- lib/recommendations.dart
+++ lib/recommendations.dart
@@ -1,5 +1,6 @@
 import 'package:buzz/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:spotify/spotify.dart' as SpotifyPack;
 
 class Recommendations extends StatefulWidget {
@@ -21,16 +22,29 @@ class _RecommendationsState extends State<Recommendations> {
         elevation: 0,
       ),
       backgroundColor: splashOrange,
-      body: ListView.builder(
-        itemCount: widget.tracks?.length ?? 0,
-        itemBuilder: (context, index) {
-          final track = widget.tracks![index];
-          return ListTile(
-            title: Text(track.name ?? ''),
-            subtitle: Text(
-                track.artists?.map((artist) => artist.name).join(', ') ?? ''),
-          );
-        },
+      body: Center(
+        child: ListView.builder(
+          itemCount: widget.tracks?.length ?? 0,
+          itemBuilder: (context, index) {
+            final track = widget.tracks![index];
+            return ListTile(
+              title: Text(
+                track.name ?? '',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: Text(
+                track.artists?.map((artist) => artist.name).join(', ') ?? '',
+                style: GoogleFonts.poppins(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
